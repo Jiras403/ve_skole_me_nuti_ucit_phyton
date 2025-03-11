5ee356eb99689becc11097b6713a33b5f47f691d
Jiras403 <jiri.siladi@infis.cz>
2025-03-11 08:41:01 +0100

    Add files via upload

---
+++ priklad.py
@@ -0,0 +1,17 @@
+# nacteni od uzivatele
+nacteni = str(input("Napis nejake ovoce: "))
+# podminky
+if nacteni == "jahoda" or nacteni == "ananas":
+    print("jou fajn ale na pizzu asi ne")
+elif nacteni == "banan":
+    print("to je banan. . . je to ovoce, které oloupete a sníte")
+else:
+    print(nacteni)
+    print("je taky super ovoce")
+
+
+
+# github prikaz
+#git add*
+#git commit -m "hodina"
+#git push
