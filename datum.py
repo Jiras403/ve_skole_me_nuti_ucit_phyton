4589ce6e7363c8d181c5c0389ad5994d6de5a70a
Jiras403 <jiri.siladi@infis.cz>
2025-03-18 08:39:14 +0100

    Add files via upload

---
+++ datum.py
@@ -0,0 +1,16 @@
+rok = 2025
+text = "vesele velikonoce"
+cislo = 159
+
+print(f"Vitejde {rok}")
+print (f"{text} {rok}")
+
+#oddeleni
+print (f"{cislo,}")
+
+#desetine
+print(f"{cislo:.5f}")
+
+#prevod
+prevod =155
+print(f"{prevod:x}")
